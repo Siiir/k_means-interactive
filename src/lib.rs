ec357cb40e2aa80a698f280c947ea626a0eae591
Tomasz Nehring <tomasz_nehring@outlook.com>
2024-05-09 20:48:13 +0200

    Centroids fixed after some unprecendental loss of code

--- src/lib.rs
+++ src/lib.rs
@@ -13,15 +13,22 @@ pub fn new_gui() -> MainWindow {
         // Pure functions
         logic.on_rand_fract_float(|| rand::thread_rng().gen());
         logic.on_rand_natural_int(|| rand::thread_rng().gen::<i32>() & i32::MAX);
-        logic.on_group_color(|rel_group_num: f32| -> slint::Color {
-            let color = palette::Hsl::new(360. * rel_group_num, 0.9, 0.5);
-            let color: palette::Srgb = color.into_color();
-            let color = slint::Color::from_rgb_f32(color.red, color.green, color.blue);
-            color
-        });
+        logic.on_group_color(group_color);
+        logic.on_group_color_with_alpha(group_color_with_alpha);
         logic.on_loosely_parse_int(crate::util::loosely_parse_int);
         // Dirty functions
         logic.on_algorithm_step(|step_count| println!("todo: perform {step_count} steps."));
     } // Return
     main_win
+}
+
+fn group_color(rel_group_num: f32) -> slint::Color {
+    group_color_with_alpha(rel_group_num, 1.)
+}
+
+fn group_color_with_alpha(rel_group_num: f32, alpha: f32) -> slint::Color {
+    let color = palette::Hsl::new(360. * rel_group_num, 0.9, 0.5);
+    let color: palette::Srgb = color.into_color();
+    let color = slint::Color::from_argb_f32(alpha, color.red, color.green, color.blue);
+    color
 }
