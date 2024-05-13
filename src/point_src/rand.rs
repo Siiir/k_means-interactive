6627a37a534ddaf12651b8d32f8e2d121d4f807f
Tomasz Nehring <tomasz_nehring@outlook.com>
2024-05-13 09:50:39 +0200

    extracted `ensure_idx_exists` as method of `IdentifiableRandPoints`

--- src/point_src/rand.rs
+++ src/point_src/rand.rs
@@ -9,6 +9,22 @@ use crate::PointSrc;
 pub struct IdentifiableRandPoints {
     observed: RefCell<Vec<[f32; 2]>>,
 }
+
+impl IdentifiableRandPoints {
+    // CRUD-U: Extenders
+    fn ensure_idx_exists(idx: usize, observed_mut: &mut std::cell::RefMut<'_, Vec<[f32; 2]>>) {
+        let mut rng;
+        loop {
+            if idx < observed_mut.len() {
+                break;
+            }
+            rng = rand::thread_rng();
+            observed_mut.push(rng.gen());
+        }
+    }
+}
+
+// CRUD-U: Extending getters
 impl PointSrc for IdentifiableRandPoints {
     /// Get the previous observation for provided `idx` or generates a new pseudo random one.
     ///
@@ -18,14 +34,7 @@ impl PointSrc for IdentifiableRandPoints {
     fn get(self: &IdentifiableRandPoints, idx: usize) -> [f32; 2] {
         let mut observed_mut = self.observed.borrow_mut();
 
-        let mut rng;
-        loop {
-            if idx < observed_mut.len() {
-                break;
-            }
-            rng = rand::thread_rng();
-            observed_mut.push(rng.gen());
-        }
+        IdentifiableRandPoints::ensure_idx_exists(idx, &mut observed_mut);
         observed_mut[idx]
     }
 }
